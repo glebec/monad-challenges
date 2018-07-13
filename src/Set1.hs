7f9d431ac561fcd44e895ad3d79ebb074e2d1940
Gabriel Lebec <glebec@gmail.com>
2018-07-12 22:55:18 -0400

    s1.3a

--- src/Set1.hs
+++ src/Set1.hs
@@ -22,8 +22,21 @@ fiveRands = map fst $ take 5 $ tail' rands
 -- 1.2
 -- toLetter :: Integer -> Char
 
-randLetter :: Seed -> (Char, Seed)
+randLetter :: Gen Char
 randLetter s = let (i, s') = rand s in (toLetter i, s')
 
 randString3 :: String
 randString3 = take 3 $ map (fst . randLetter . snd) rands
+
+-- 1.3
+
+type Gen a = Seed -> (a, Seed)
+
+randEven :: Gen Integer -- the output of rand * 2
+randEven s = let (n, s') = rand s in (n * 2, s')
+
+randOdd :: Gen Integer -- the output of rand * 2 + 1
+randOdd s = let (n, s') = randEven s in (n + 1, s')
+
+randTen :: Gen Integer -- the output of rand * 10
+randTen s = let (n, s') = rand s in (n * 10, s')
