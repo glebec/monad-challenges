4a93fc4192116e9e99ee6bd4a3bc5b49cd064ba8
Gabriel Lebec <glebec@gmail.com>
2018-07-12 20:39:46 -0400

    s1.2

--- src/Set1.hs
+++ src/Set1.hs
@@ -5,10 +5,25 @@ module Set1 where
 
 import MCPrelude
 
+-- 1.1
+-- rand :: Seed -> (Integer, Seed)
+-- mkSeed :: Integer -> Seed
+
 tail' :: [a] -> [a]
 tail' [] = []
 tail' (x:xs) = xs
 
+rands :: [(Integer, Seed)]
+rands = (0, mkSeed 1) : map (rand . snd) rands
+
 fiveRands :: [Integer]
 fiveRands = map fst $ take 5 $ tail' rands
-  where rands = (0, mkSeed 1) : map (rand . snd) rands
+
+-- 1.2
+-- toLetter :: Integer -> Char
+
+randLetter :: Seed -> (Char, Seed)
+randLetter s = let (i, s') = rand s in (toLetter i, s')
+
+randString3 :: String
+randString3 = take 3 $ map (fst . randLetter . snd) rands
