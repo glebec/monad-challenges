681fe2d4b4ae2a922f6325ad5d8650aec9653602
Gabriel Lebec <glebec@gmail.com>
2018-07-15 19:43:10 -0400

    s3.5

--- src/Set3.hs
+++ src/Set3.hs
@@ -46,3 +46,16 @@ allCombs3 _ _ [] _ = []
 allCombs3 _ _ _ [] = []
 allCombs3 f lx@(x:xs) ly lz = let fxys = map (f x) ly in
     allCombs ($) fxys lz ++ allCombs3 f xs ly lz
+
+-- 3.5
+
+combStep :: [a -> b] -> [a] -> [b]
+combStep [] _ = []
+combStep _ [] = []
+combStep (f:fs) as = map f as ++ combStep fs as
+
+allCombs' :: (a -> b -> c) -> [a] -> [b] -> [c]
+allCombs' f = combStep . combStep [f]
+
+allCombs3' :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
+allCombs3' f as bs = combStep (combStep (combStep [f] as) bs)
