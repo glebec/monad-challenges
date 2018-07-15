4b48c8d4499558f4f65d40c4033931f3f7d292c3
Gabriel Lebec <glebec@gmail.com>
2018-07-15 00:24:34 -0400

    s2.2

--- src/Set2.hs
+++ src/Set2.hs
@@ -11,3 +11,27 @@ data Maybe a = Nothing | Just a deriving (Eq) -- normally would derive Show
 instance Show a => Show (Maybe a) where
     show Nothing = "Nothing"
     show (Just a) = "Just " ++ show a
+
+headMay :: [a] -> Maybe a
+headMay [] = Nothing
+headMay (x:xs) = Just x
+
+tailMay :: [a] -> Maybe [a]
+tailMay [] = Nothing
+tailMay (x:xs) = Just xs
+
+lookupMay :: Eq a => a -> [(a, b)] -> Maybe b
+lookupMay _ [] = Nothing
+lookupMay a [(a', b)] = if a == a' then Just b else Nothing
+
+divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
+divMay _ 0 = Nothing
+divMay a b = Just $ a / b
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay [] = Nothing
+maximumMay xs = Just $ foldr1 max xs
+
+minimumMay :: Ord a => [a] -> Maybe a
+minimumMay [] = Nothing
+minimumMay xs = Just $ foldr1 min xs
