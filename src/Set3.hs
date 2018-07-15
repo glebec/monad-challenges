54887abade08beb8edd240f9b79ae69517b87131
Gabriel Lebec <glebec@gmail.com>
2018-07-15 17:41:34 -0400

    s3.2

--- src/Set3.hs
+++ src/Set3.hs
@@ -5,8 +5,22 @@ module Set3 where
 
 import MCPrelude
 
+-- 3.1
+
 allPairs :: [a] -> [b] -> [(a,b)]
 -- allPairs xs ys = [(x, y) | x <- xs, y <- ys]
 allPairs [] _  = []
 allPairs _  [] = []
 allPairs (x:xs) l2 = map ((,) x) l2 ++ allPairs xs l2
+
+-- 3.2
+
+data Card = Card Int String deriving (Eq)
+
+instance Show Card where
+    show (Card rank suit) = show rank ++ suit
+
+allCards :: [Int] -> [String] -> [Card]
+allCards [] _ = []
+allCards _ [] = []
+allCards (r:rs) suits = map (Card r) suits ++ allCards rs suits
