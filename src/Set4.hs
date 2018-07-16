29c5494cec1a6afa320471a28280c9cbb0b0a4e2
Gabriel Lebec <glebec@gmail.com>
2018-07-16 00:10:01 -0400

    s4.2

--- src/Set4.hs
+++ src/Set4.hs
@@ -4,6 +4,7 @@
 module Set4 where
 
 import MCPrelude
+import Set1 (Gen, generalA, mkGen, genTwo)
 
 -- 4.1
 
@@ -24,3 +25,19 @@ import MCPrelude
 -- yLinkM   :: (a -> b -> c) -> m     a -> m     b -> m     c
 -- generalB :: (a -> b -> c) -> Gen   a -> Gen   b -> Gen   c
 -- yLink    :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+
+-- 4.2
+
+generalB2 :: (a -> b -> c) -> Gen a -> Gen b -> Gen c
+generalB2 f ga gb = ga `genTwo` (\a -> gb `genTwo` (mkGen . f a))
+
+-- mkGen :: a -> Gen a
+-- genTwo :: Gen a -> (a -> Gen b) -> Gen b
+-- generalA :: (a -> b) -> Gen a -> Gen b
+repRandom2 :: [Gen a] -> Gen [a]
+repRandom2 [] = mkGen []
+repRandom2 (g:gs) =
+    g `genTwo` (\a ->
+        repRandom2 gs `genTwo` (\as ->
+            mkGen $ a : as))
+-- well THAT was mind-bending, and I didn't use `generalA`, so now I'm worried.
